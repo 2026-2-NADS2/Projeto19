3a3bc7a40a9423d9fc4163dac755b8ebe9b13d69
Ericcx06 <ericcarvalho.1e@gmail.com>
2026-09-25 20:49:42 -0300

    coloquei o iniciar.bat no local certo (pasta certa)

--- src/FullStack/Frontend/iniciar.bat
+++ src/FullStack/Frontend/iniciar.bat
@@ -10,15 +10,12 @@ where npm >nul 2>nul
 if %errorlevel% neq 0 (
     echo ERRO: Node.js nao foi encontrado.
     echo Instale o Node.js para executar o projeto.
-    echo.
     pause
     exit
 )
 
 if not exist node_modules (
     echo Instalando dependencias...
-    echo Isso pode levar alguns minutos.
-    echo.
     call npm install
 )
 
